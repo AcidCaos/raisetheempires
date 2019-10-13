ee03035d5ceddc1925f01376f2ba7900a77fc19d
thebatmanwa <swapnilantala99@gmail.com>
2019-10-13 19:29:16 +0530

    Revert "Flag_bug_solved"
    This reverts commit eb41650

--- quest_settings.py
+++ quest_settings.py
@@ -6,5 +6,4 @@ from save_engine import my_games_path
 with open(os.path.join(my_games_path() ,"questsettings-converted.json"), 'r') as f:
     quest_settings = json.load(f)
     print("Questsettings loaded: ",  len(quest_settings['quests']['quest']), " quests loaded")
-    print(quest_settings['quests']['quest'][5]['_name'])
-    print(quest_settings['quests']['quest'][5])
+
