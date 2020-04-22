09d585020cc74957d1a029ce7792a6af4d3ea086
Oshere1111 <oshere1111@gmail.com>
2020-04-22 16:43:15 +0300

    adding crewInfo when migrate

--- save_migration.py
+++ save_migration.py
@@ -1,5 +1,5 @@
 from flask import session
-
+from save_engine import lookup_object
 from game_settings import unlock_expansion
 from quest_engine import handle_quest_progress
 from save_engine import create_backup
@@ -32,13 +32,43 @@ def migrate(meta, version, target_version):
         create_backup("Update to 0.05a")
         version = "0.05a"
         session['save_version'] = version
-    if version == "0.05a" and version != target_version: # upcoming 0.06
+    if  version.startswith("0.05a") and version != target_version: # upcoming 0.06
+        #create_backup("Update to 0.06a")#  for release
+        #version = "0.06a"
+        #govt_fixer()
+        session['user_object']["experiments"]["empire_decorations_master"] = 2
+        #session['save_version'] = version
+    if version == "0.06a" and version != target_version: # upcoming 0.07
         # create_backup("Update to 0.06a") # for release
-        # version = "0.06a"
+        # version = "0.07a"
         #
         session['user_object']["experiments"]["empire_decorations_master"] = 2
+
+
 
 def zero_yield(extra):
     extra["yield"] = 0
     return True
+
+
+def govt_fixer():
+    govt_staff = {"Parliament": 3,
+                  "Executive Mansion": 4,
+                  "Federal Courthouse": 4,
+                  "Treasury": 5,
+                  "Maritime Academy": 6,
+                  "National Archives": 7,
+                  "Federal Prison": 8,
+                  "State Media": 10,
+                  "Opera House": 11,
+                  "Mint": 12,
+                  }
+
+    for decoration in session['user_object']["userInfo"]["world"]["objects"]:
+        if "crewInfo" not in decoration and decoration["itemName"] in govt_staff \
+            and int(decoration.get("state")) >= 8 :
+
+            for reapet_number in range(govt_staff.get(decoration["itemName"])):
+                decoration["crewInfo"] = decoration.get("crewInfo", []) + [-1]
+
 
