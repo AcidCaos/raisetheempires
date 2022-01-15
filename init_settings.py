33f2b1853338c1b2124e0c54e85af91ff5683228
AcidCaos <acidcaos1@gmail.com>
2022-01-15 21:30:23 +0100

    Fix: handle invalid command arguments.

--- init_settings.py
+++ init_settings.py
@@ -1,7 +1,11 @@
 import getopt
 import sys
 
-opts, args = getopt.getopt(sys.argv[1:], "", ["debug", "host=", "port=", "http-host=", "http-path=", "no-popup", "no-crash-log", "no-compression", "no-caching", "no-app_mode"])
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "", ["debug", "host=", "port=", "http-host=", "http-path=", "no-popup", "no-crash-log", "no-compression", "no-caching", "no-app_mode"])
+except getopt.GetoptError as e:
+    print("Error:", e.msg)
+    exit(1)
 
 debug = False
 host = '127.0.0.1'  # host to listen on 0.0.0.0 for all interfaces, 127.0.0.1 for only localhost
@@ -35,5 +39,3 @@ for o, a in opts:
         caching = False
     elif o == '--no-app_mode':
         app_mode = False
-    else:
-        print("Warning: Unknown option " + o + ".")
